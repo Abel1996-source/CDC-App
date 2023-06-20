7545df389294595e4b7d9b3ad09e9e9f32374b89
konan kouakou Abel <k.kouakouabel96@gmail.com>
2023-06-20 15:59:04 +0200

    ajout de breadcrumb dans le composant overview

--- src/contents/Overview.jsx
+++ src/contents/Overview.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
 
 const Overview=()=>{
     return(
         <>
             <div className="overview">
-                 <p>Welcome in your App CDC Overview</p>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><Link to="/dashboad">Dashboad</Link></li>
+                        <li class="breadcrumb-item active" aria-current="page">Overview</li>
+                    </ol>
+                </nav>
             </div>
         </>
     )
