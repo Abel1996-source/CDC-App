b66a0b8a9185d3e77fc8c4bce99c657482d4bad8
konan kouakou Abel <k.kouakouabel96@gmail.com>
2023-06-29 19:06:50 +0200

    page register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,38 @@
-import { Button } from "@mui/material";
+
 import { Link } from "react-router-dom";
 
 const Register=()=>{
     return(
         <>
-            <div className="register">
-               <Link to="/dashboad"> <Button>Go to dashboad</Button></Link>
+         < div className="content-login">
+                <div className="logo">
+                    <span>Register</span>
+                </div>
+                <div className="login-left">
+                    <div id="emailHelp" className="form-text"></div>
+                    <form>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Nom</label>
+                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ex: Konan"/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Prenom</label>
+                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ex: Kouakou "/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputEmail1" className="form-label">Email</label>
+                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Ex: y@gmail.com"/>
+                        </div>
+                        <div className="mb-3">
+                            <label for="exampleInputPassword1" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Ex: chccdgPO986"/>
+                        </div>
+                        <button type="submit" className="btn btn-warning form-control">Enregistrer</button>
+                    </form>
+                </div>
+                <div className="regist">
+                        <span>Vous avez déjà un compte utilisateur? <Link to="/login">Connexion</Link></span>
+                </div>
             </div>
         </>
     )
