97b76af1b5d7db915ebf066962e6709ca17f193a
konan kouakou Abel <k.kouakouabel96@gmail.com>
2023-07-03 16:03:36 +0200

    Mise en place du composent customers

--- src/contents/Customers.jsx
+++ src/contents/Customers.jsx
@@ -11,6 +11,20 @@ const Customers=()=>{
                     </ol>
                 </nav>
                 <hr />
+                <div className="content-users">
+                    <div className="users-header">
+                        <button className="btn btn-primary">Add</button>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        </form>
+                    </div>
+                    <div className="users-body">
+                        <div className="users">
+                            <span>k.kouakouabel96@gmail.com</span>
+                            <span className="btn btn-light deleted"><i className="bi bi-trash-fill"></i></span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
