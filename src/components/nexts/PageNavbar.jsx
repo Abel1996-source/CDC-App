cb47fbc5cf3810bbda2b3083590818c4424669b1
konan kouakou Abel <k.kouakouabel96@gmail.com>
2023-07-28 12:14:46 +0200

    mise à jour de la page home

--- src/components/nexts/PageNavbar.jsx
+++ src/components/nexts/PageNavbar.jsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const PageNavbar=()=>{
@@ -8,10 +9,11 @@ const PageNavbar=()=>{
                 <div>
                     <Link to="/" className="logo-a"><h3 className="float-md-start mb-0 logo">Asyé</h3></Link>
                     <nav className="nav nav-masthead justify-content-center float-md-end">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         <Link className="nav-link" to="#">Blog</Link>
-                        <Link className="nav-link" to="/login">Connexion</Link>
-                        <Link className="nav-link" to="#">Services</Link>
+                        <Link className="nav-link" to="#">Services</Link>                
+                        <Tooltip title="login">
+                            <Link className="nav-link active" aria-current="page" to="/login"><i className="bi bi-person-check-fill"></i></Link>
+                        </Tooltip>
                         
                     </nav>
                     </div>
