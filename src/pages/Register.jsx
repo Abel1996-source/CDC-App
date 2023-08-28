eeb2b6e7fccdcf1300405c7e9d64ae13e9cbed6e
konan kouakou Abel <k.kouakouabel96@gmail.com>
2023-08-28 15:41:36 +0200

    actualiser

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -44,7 +44,7 @@ const Register=()=>{
             console.log(error);
             setRegiste(false)
             setErrorregiste(true)
-            setError(error.response.data.message)
+           // setError(error.response.data.message)
             setTimeout(function() {
                 setErrorregiste(false)
               }, 3000);
